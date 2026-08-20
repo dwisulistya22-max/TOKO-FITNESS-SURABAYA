f5077ff0b4c34c7c66dc20214bfc5f47e5f62cc2
dwisulistya22-max <dwisulistya22@gmail.com>
2026-08-20 22:44:37 +0700

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -3,7 +3,7 @@ export const STORE_CONFIG = {
   logo: "https://i.ibb.co/Lhb85T8/Surabaya-Fitness-Logo.jpg?v=2",
   slogan: "Kualitas Gym Profesional, Di Rumah Anda",
   phone: "6281332345448",
-  email: "dwisulistya22@gmail.com
+  email: "dwisulistya22@gmail.com",
 ",
   address: "Jl. Kuwukan Gg. 2 No.22, Lontar, Kec. Sambikerep, Surabaya, Jawa Timur 60216",
   hero: {
