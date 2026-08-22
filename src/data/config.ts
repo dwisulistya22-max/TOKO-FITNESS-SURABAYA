98e7fee77f6d674ee7abe5d32b0ca8ea49e06be7
dwisulistya22-max <dwisulistya22@gmail.com>
2026-08-23 00:15:55 +0700

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,9 +1,10 @@
 export const STORE_CONFIG = {
   name: "TOKO FITNESS SURABAYA",
-  logo: "",
+  // Logo Default FS Metalik Biru Keren jika di Sanity belum diisi
+  logo: "https://images.unsplash.com/photo-1517836357463-d25dfeac3438?q=80&w=800&auto=format&fit=crop",
   slogan: "Kualitas Gym Profesional Di Rumah Anda",
-  phone: "6281235907956, 6281332345448",
-  phone2: "6281332345448",
+  phone: "6281332345448, 6281235907956",
+  phone2: "6281235907956",
   email: "dwisulistya22@gmail.com",
   address: "Jl. Kuwukan Gg. 2 No.22, Lontar, Kec. Sambikerep, Surabaya, Jawa Timur 60216",
   shopee: "https://shopee.co.id/search?keyword=toko%20fitness%20surabaya",
