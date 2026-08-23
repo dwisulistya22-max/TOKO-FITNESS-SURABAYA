a97da0789bc0329b0ef4b335f7a1401a29c43eea
dwisulistya22-max <dwisulistya22@gmail.com>
2026-08-23 11:37:35 +0700

    Create index.ts

---
+++ src/src/schemaTypes/src/schemaTypes/index.ts
@@ -0,0 +1,13 @@
+import category from './category';
+import product from './product';
+import storeConfig from './storeConfig';
+import slider from './slider';
+import testimonial from './testimonial';
+
+export const schemaTypes = [
+  category,
+  product,
+  storeConfig,
+  slider,
+  testimonial
+];
