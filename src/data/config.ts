fcbf50ffc749632b7702b21593e0c7fc861b9b4a
dwisulistya22-max <dwisulistya22@gmail.com>
2026-08-23 12:37:53 +0700

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -6,7 +6,8 @@ export const STORE_CONFIG = {
   phone2: "6281235907956",
   email: "dwisulistya22@gmail.com",
   address: "Jl. Kuwukan Gg. 2 No.22, Lontar, Kec. Sambikerep, Surabaya, Jawa Timur 60216",
-  shopee: "https://shopee.co.id/search?keyword=toko%20fitness%20surabaya",
+  shopee: "https://id.sh.ee/PEdSUDy6",
+  tokopedia: "https://www.tokopedia.com/search?st=product&q=toko%20fitness%20surabaya",
   hero: {
     title: "KUALITAS GYM PROFESIONAL DI RUMAH ANDA",
     subtitle: "Pusat penyedia alat fitness terlengkap dan terpercaya di Surabaya.",
@@ -14,7 +15,6 @@ export const STORE_CONFIG = {
   }
 };
 
-// 🌟 DAFTAR ULASAN PEMBELI (BISA ANDA EDIT KATA-KATANYA DI SINI KAPAN SAJA)
 export const TESTIMONIALS = [
   {
     name: "Budi Santoso",
